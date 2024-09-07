d250d76625c51d5c7457ad222adb31585bb8f0d3
Anthony Heitzeberg <a.heitzeberg007@gmail.com>
2024-09-07 14:22:41 -0500

    create model of entities

---
+++ src/main/java/com/dailycodework/dream_shops/model/Image.java
@@ -0,0 +1,11 @@
+package com.dailycodework.dream_shops.model;
+
+import java.sql.Blob;
+
+public class Image {
+    private Long id;
+    private String filename;
+    private String filetype;
+    private Blob image;
+    private String downloadUrl;
+}
